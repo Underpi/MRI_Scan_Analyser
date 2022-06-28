dc46c0ae7b8d7885d16b97479945758dcdd79e34
GoldPineapple <67350143+Akwak44@users.noreply.github.com>
2022-06-27 21:11:01 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='MRI_Scan_Analyser',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='',
+    author='Arjun Krish',
+    author_email='arjunbkrish@gmail.com',
+    description='Analyze MRI scans for "defects"'
+)
